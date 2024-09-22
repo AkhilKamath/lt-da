a3c13df20dc2bab3569d887b91dab806150fcaf5
AkhilKamath <akhilkamath97@gmail.com>
2024-09-22 21:59:19 +0530

    minor changes

--- programs/linktree/src/lib.rs
+++ programs/linktree/src/lib.rs
@@ -1,4 +1,4 @@
-use anchor_lang::prelude::*;
+use anchor_lang::{prelude::*, solana_program::address_lookup_table::instruction};
 
 declare_id!("6jnvkMV423aCV52ieVPpfXwj3oWR8wKbR275pjNKdvgZ");
 
@@ -8,6 +8,8 @@ declare_id!("6jnvkMV423aCV52ieVPpfXwj3oWR8wKbR275pjNKdvgZ");
 #[derive(InitSpace)]
 pub struct LinkTreeAccount {
   pub owner: Pubkey,
+  #[max_len(20)]
+  pub username: String,
   #[max_len(10)]
   pub links: Vec<Link>,
   pub id: u64
@@ -26,13 +28,27 @@ pub struct Link {
 pub mod linktree {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
-        Ok(())
+    pub fn create_linktree_account(
+      ctx: Context<CreateLinkTreeAccount>,
+    ) {
+
     }
 }
 
 
 
 #[derive(Accounts)]
-pub struct Initialize {}
+#[instruction(username: String)]
+pub struct CreateLinkTreeAccount<'info> {
+  #[account(
+    init,
+    seeds = [username.as_bytes(), owner.key().as_ref()],
+    bump,
+    payer = owner,
+    space = 8 + LinkTreeAccount::INIT_SPACE,
+  )]
+  pub linktree_account: Account<'info, LinkTreeAccount>,
+  #[account(mut)]
+  pub owner: Signer<'info>,
+  pub system_program: Program<'info, System>
+}
